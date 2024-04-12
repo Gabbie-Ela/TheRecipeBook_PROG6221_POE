0360b4f4f59f2c872bc5dad6a030b5443052e66d
Nicol Nangura Gabriela Black <130062264+Gabbie-Ela@users.noreply.github.com>
2024-04-12 16:53:41 +0000

    Add files via upload

---
+++ Recipe.cs
@@ -0,0 +1,23 @@
+﻿using RecipeBook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Define the namespace for the program, which encapsulates the RecipeManager class
+namespace RecipeBook
+{
+    // Define an internal class named Recipe to represent a recipe
+    internal class Recipe
+    {
+        // Public property to store the name of the recipe
+        public string Name { get; set; }
+
+        // Public property to store an array of ingredients in the recipe
+        public Ingredient[] Ingredients { get; set; }
+
+        // Public property to store an array of steps in the recipe
+        public Step[] Steps { get; set; }
+    }
+}
---
+++ Step.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Define the namespace for the program, which encapsulates the RecipeManager class
+namespace RecipeBook
+{
+    // Define an internal class named Step to represent a step in a recipe
+    internal class Step
+    {
+        // Public property to store the description of the step
+        public string Description { get; set; }
+    }
+}
